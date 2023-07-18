e767e181aea87e2d8cd263d8a3879745f3bb8c33
Bryan Haakman <bryanhaakman@gmail.com>
2023-07-18 09:47:01 +0200

    Add available wifi networks (#16)

--- src/cic-detail/CICDetailNetworkConnection.tsx
+++ src/cic-detail/CICDetailNetworkConnection.tsx
@@ -1,8 +1,10 @@
-import { AdminCic } from "../api-client/models";
+import React from "react";
+import { AdminCic, CicAvailableWifiNetworksInner } from "../api-client/models";
 import { FormField, FormFieldTitle, FormFieldValue, FormSection } from "../ui-components/form/Form";
 import { formatDateDistance } from "../utils/formatDate";
 import classes from "./CICDetail.module.css";
 import { CICDetailSectionHeader } from "./CICDetailSectionHeader";
+import { Accordion, AccordionItem } from "../ui-components/accordion/Accordion";
 
 export function CICDetailNetworkConnection({
   cicData
@@ -45,7 +47,64 @@ export function CICDetailNetworkConnection({
           <FormFieldTitle>Is scanning for Wifi</FormFieldTitle>
           <FormFieldValue value={cicData.isScanningForWifi} />
         </FormField>
+        <FormField>
+          <FormFieldTitle>
+            Last scanned for Wifi
+          </FormFieldTitle>
+          <FormFieldValue
+            value={formatDateDistance(cicData.lastScannedForWifi)}
+          />
+        </FormField>
+
+        {cicData.availableWifiNetworks && (
+          <Accordion>
+            {cicData.availableWifiNetworks.map((data, index) => (
+              <AvailableWifiNetwork key={index} data={data} index={index+1} />
+            ))}
+          </Accordion>
+        )}
       </FormSection>
     </div>
   )
+}
+
+
+function AvailableWifiNetwork({
+  index,
+  data
+}: {
+  index: number;
+  data: CicAvailableWifiNetworksInner
+}) {
+  const [isOpen, setIsOpen] = React.useState(false)
+  return (
+    <AccordionItem
+      title={`Available network ${index}: ${data.sSID}`}
+      isOpen={isOpen}
+      onChangeIsOpen={() => setIsOpen(!isOpen)}
+    >
+      <FormSection>
+        <FormField>
+          <FormFieldTitle>Status</FormFieldTitle>
+          <FormFieldValue value={data.sSID} />
+        </FormField>
+        <FormField>
+          <FormFieldTitle>Signal</FormFieldTitle>
+          <FormFieldValue value={data.signal} />
+        </FormField>
+        <FormField>
+          <FormFieldTitle>Security</FormFieldTitle>
+          <FormFieldValue value={data.security} />
+        </FormField>
+        <FormField>
+          <FormFieldTitle>Encrypted</FormFieldTitle>
+          <FormFieldValue value={data.encrypted} />
+        </FormField>
+        <FormField>
+          <FormFieldTitle>Bars out of 5</FormFieldTitle>
+          <FormFieldValue value={data.barsOutOf5} />
+        </FormField>
+      </FormSection>
+    </AccordionItem>
+  );
 }
