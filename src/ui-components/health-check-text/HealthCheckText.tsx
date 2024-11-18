524b9557fb52480565c31652e230e3814fbb25f2
Mikael Labrut <labrut@gmail.com>
2024-11-18 11:54:29 +0100

    feat: add healthcheck on CIC for number of restarts

--- src/ui-components/health-check-text/HealthCheckText.tsx
+++ src/ui-components/health-check-text/HealthCheckText.tsx
@@ -4,17 +4,11 @@ import Tooltip from "../tooltip/Tooltip";
 
 interface HealthCheckTextProps {
   title: string;
+  text: string;
   status: CicHealthCheckStatus;
   errorStatusText: string;
   notApplicableStatusText?: string;
 }
-
-const connectionTextByStatus: Record<CicHealthCheckStatus, string> = {
-  [CicHealthCheckStatus.Correct]: "Connected",
-  [CicHealthCheckStatus.Error]: "Disconnected",
-  [CicHealthCheckStatus.Warning]: "Warning",
-  [CicHealthCheckStatus.NotApplicable]: "N/A",
-};
 
 const colorsByStatus: Record<CicHealthCheckStatus, string> = {
   [CicHealthCheckStatus.Correct]: "green",
@@ -26,12 +20,12 @@ const colorsByStatus: Record<CicHealthCheckStatus, string> = {
 function HealthCheckText({
   title,
   status,
+  text,
   errorStatusText,
   notApplicableStatusText = `
     Can't get the status.
     Either the CIC's internet connection is bad or disconnected, or we're missing the related value.`,
 }: HealthCheckTextProps) {
-  const connectedText = connectionTextByStatus[status];
   const isStatusNotApplicable = status === CicHealthCheckStatus.NotApplicable;
   const isStatusError = status === CicHealthCheckStatus.Error;
   const showTooltip = isStatusNotApplicable || isStatusError;
@@ -50,7 +44,7 @@ function HealthCheckText({
       <Tooltip text={tooltipText} showTooltip={showTooltip}>
         <div className={classes["health-check-container"]}>
           <h3 className={classes["health-check-title"]}>{title}:</h3>
-          <span style={{ color: colorsByStatus[status] }}>{connectedText}</span>
+          <span style={{ color: colorsByStatus[status] }}>{text}</span>
         </div>
       </Tooltip>
     </div>
